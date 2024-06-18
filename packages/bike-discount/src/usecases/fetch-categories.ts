9485b392f24165a46545e2940bd46e64e5cfb01b
Lucas Barbosa <barbosa.lucash@gmail.com>
2024-06-18 15:57:47 -0300

    chore: generate categories after fetching them

--- packages/bike-discount/src/usecases/fetch-categories.ts
+++ packages/bike-discount/src/usecases/fetch-categories.ts
@@ -1,5 +1,6 @@
 import { listCategories } from '@crawler/actions/list-categories';
 import { saveCategories } from '@infrastructure/categories';
+import { generateCategoriesTree } from './generate-categories-tree';
 
 export const fetchCategories = async () => {
   const categories = await listCategories().catch(err => {
@@ -7,6 +8,10 @@ export const fetchCategories = async () => {
     return null;
   });
 
-  if (categories) await saveCategories(categories);
+  if (categories) {
+    await saveCategories(categories);
+    await generateCategoriesTree();
+  }
+
   return categories;
 };
