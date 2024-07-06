941dd9683f19bea6c2d7566e4286657c86df14c4
Lucas Barbosa <barbosa.lucash@gmail.com>
2024-07-06 17:01:53 -0300

    chore: add action to list categories on barrabes crawler

---
+++ packages/barrabes/src/crawler/actions/list-categories.ts
@@ -0,0 +1,98 @@
+import axios from 'axios';
+import { load } from 'cheerio';
+import { type Category } from '@crawlers/base/dist/types/Category';
+
+interface BarrabesCategory extends Category {
+  isParent?: boolean
+}
+
+export const listCategories = async (): Promise<Category[]> => {
+  const url = 'https://www.barrabes.com/pt';
+  const { data } = await axios.get(url);
+  const $ = load(data);
+
+  const elements = $('#menu-inferior-ul li:not(.nav-brands) button.nav-anchor, #menu-inferior-ul .nav--level-2 .nav-link a');
+
+  const promises = elements.map(async (i, element): Promise<BarrabesCategory> => {
+    const $element = $(element);
+    const isParent = $element.is('button');
+    const name = $element.text().trim();
+    const url = $element.attr('href');
+
+    if (isParent || !url) {
+      return {
+        name,
+        isParent,
+        url: '',
+        childs: []
+      };
+    }
+    const formattedUrl = addPrefixIfRelative(url);
+    return {
+      name,
+      url: formattedUrl,
+      childs: await getSubcategories(formattedUrl)
+    };
+  });
+
+  const categories = await Promise.all(promises);
+  return makeCategoriesTree(categories);
+};
+
+export const listProCategories = async (): Promise<Category[]> => {
+  const url = 'https://www.barrabes.com/gama-profesionales/c-1406';
+  const categories = await getSubcategories(url);
+  return makeCategoriesTree(categories);
+};
+
+const addPrefixIfRelative = (url: string) => {
+  if (url.startsWith('http://') || url.startsWith('https://')) {
+    return url;
+  }
+  return `https://www.barrabes.com${url}`;
+};
+
+const getSubcategories = async (subcategoryUrl: string) => {
+  const { data } = await axios.get(subcategoryUrl);
+  const $ = load(data);
+  const elements = $('#filter-cat a.lnkCat, #filter-cat ul.subc a');
+  const promises = elements.map(async (i, element): Promise<BarrabesCategory> => {
+    const $element = $(element);
+    const name = $element.text().trim();
+    const url = $element.attr('href');
+    const formattedUrl = addPrefixIfRelative(url ?? '');
+    const isParent = $element.hasClass('cat');
+    const childs = !$element.hasClass('lnkCat') ? await getSubcategories(formattedUrl) : [];
+    return {
+      name,
+      isParent,
+      childs,
+      url: formattedUrl
+    };
+  });
+  return Promise.all(promises);
+};
+
+const makeCategoriesTree = (categories: BarrabesCategory[]) => {
+  const categoriesTree: Category[] = [];
+  let categoriesTemp = [];
+
+  for (let i = categories.length - 1; i >= 0; i--) {
+    const barrabesCategory = categories[i];
+    const category = {
+      url: barrabesCategory.url,
+      childs: barrabesCategory.childs,
+      name: barrabesCategory.name
+    };
+
+    if (barrabesCategory.isParent) {
+      category.childs = categoriesTemp;
+      categoriesTemp = [];
+      categoriesTree.unshift(category);
+    } else {
+      categoriesTemp.unshift(category);
+    }
+  }
+
+  return categoriesTree;
+};
