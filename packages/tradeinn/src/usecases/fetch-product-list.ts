e5d4696d4d8af3b538f8d3237f824a4e6726b516
Lucas Barbosa <barbosa.lucash@gmail.com>
2024-08-08 12:05:03 -0300

    chore: add fetch tradeinn product list

--- packages/tradeinn/src/usecases/fetch-product-list.ts
+++ packages/tradeinn/src/usecases/fetch-product-list.ts
@@ -1,13 +1,27 @@
-// import { listProducts } from '@crawler/actions/list-products';
+import { listProducts } from '@crawler/actions/list-products';
 
 export const fetchProductList = async (categoryUrl: string, page: number = 1) => {
-  return {
-    productLinks: [],
-    hasNextPage: false
-  };
-  //  await listProducts(categoryUrl, page)
-  //   .catch(err => {
-  //     console.warn(err);
-  //     return null;
-  //   });
+  const params = getParamsFromUrl(categoryUrl);
+
+  if (!params) {
+    return null;
+  }
+
+  return listProducts(params.parentId, params.categoryId, categoryUrl, page)
+    .catch(err => {
+      console.warn(err);
+      return null;
+    });
+};
+
+const getParamsFromUrl = (url: string) => {
+  const urlObj = new URL(url);
+  const parentId = urlObj.searchParams.get('parentId');
+  const categoryId = urlObj.searchParams.get('categoryId');
+
+  if (parentId === null || categoryId === null) {
+    return null;
+  }
+
+  return { parentId, categoryId };
 };
