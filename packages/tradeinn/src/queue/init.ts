a59629446370556cc39c44d1bd49754502bbda67
Lucas Barbosa <barbosa.lucash@gmail.com>
2024-08-15 15:10:09 -0300

    chore: add queue to get tradeinn product images

--- packages/tradeinn/src/queue/init.ts
+++ packages/tradeinn/src/queue/init.ts
@@ -4,10 +4,12 @@ import { categoryQueue, startCategoryQueue } from './category';
 import { startProductQueue, productQueue } from './product';
 import { startStockQueue, stockQueue } from './stock';
 import { startTranslationQueue, translationQueue } from './translate';
+import { productImageQueue, startProductImageQueue } from './product-image';
 
 export const initQueue = async ({
   onCategoriesFound,
   onProductFound,
+  onProductImageFound,
   onStockFound,
   onTranslationFound
 }: QueueParams) => {
@@ -16,12 +18,14 @@ export const initQueue = async ({
   startProductQueue(onProductFound);
   startTranslationQueue(onTranslationFound);
   startCategoryQueue();
+  if (onProductImageFound) startProductImageQueue(onProductImageFound);
 };
 
 export const queues = () => {
   return [
     stockQueue(),
     productQueue(),
+    productImageQueue(),
     categoriesQueue(),
     categoryQueue(),
     translationQueue()
