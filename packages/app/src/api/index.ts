265158eb2c98f9d91bf17b4baf24e65c99a22069
Lucas Barbosa <barbosa.lucash@gmail.com>
2024-08-21 15:16:36 -0300

    chore: increase body limit

--- packages/app/src/api/index.ts
+++ packages/app/src/api/index.ts
@@ -20,7 +20,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(session({ secret: AUTH.secret, saveUninitialized: true, resave: true }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(passport.authenticate('session'));
 
