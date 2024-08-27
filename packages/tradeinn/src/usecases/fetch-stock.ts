c209b5b47c3c5d2665838d556ea496f791d3a37c
Lucas Barbosa <barbosa.lucash@gmail.com>
2024-08-27 16:40:36 -0300

    chore: add sleep on fetch tradeinn stock

--- packages/tradeinn/src/usecases/fetch-stock.ts
+++ packages/tradeinn/src/usecases/fetch-stock.ts
@@ -1,8 +1,10 @@
 import { getProductStock } from '@crawler/actions/get-product-stock';
+import { type ProductStock } from '@crawlers/base/dist/types/ProductStock';
 
 export const fetchStock = async (productUrl: string) => {
   return await getProductStock(productUrl)
-    .then(res => res.stock)
+    // Add timeout to avoid being detected by rate limit
+    .then(res => new Promise<ProductStock>(resolve => setTimeout(resolve, Math.floor(Math.random() * (2000 - 1000 + 1)) + 1000, res.stock)))
     .catch(err => {
       console.warn(err);
       return null;
